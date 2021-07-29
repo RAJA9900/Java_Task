a169f115d8ed44403e11807a246d6346b9cd0e1a
RAJA@9900 <raja9900ganesh@gmail.com>
2021-07-29 08:10:24 +0530

    Tasks

---
+++ Javatasks/src/july_28/Third_task.java
@@ -0,0 +1,12 @@
+package july_28;
+import java.lang.Math;
+
+public class Third_task {
+	
+	public static void main (String[] args)
+	{
+		double mass_of_earth = 5.972*( Math.pow(10, 24));
+		System.out.printf("%.0f",mass_of_earth);
+	}
+  
+}
